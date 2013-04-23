2056875b4cef92504dcbf5800b2e865ba48c1b66
sernst <swernst@gmail.com>
2013-04-23 00:15:34 -0700

    Adds basic navigation to the recent container path selection widget.

--- src/nwm/views/recent/NwmRecentWidget.py
+++ src/nwm/views/recent/NwmRecentWidget.py
@@ -4,12 +4,13 @@
 
 from PySide import QtGui
 
+from pyglass.elements.buttons.PyGlassPushButton import PyGlassPushButton
+from pyglass.elements.icons.IconSheetMap import IconSheetMap
+from pyglass.enum.SizeEnum import SizeEnum
+from pyglass.themes.ColorSchemes import ColorSchemes
 from pyglass.widgets.PyGlassWidget import PyGlassWidget
 
 from nwm.enum.AppConfigEnum import AppConfigEnum
-from nwm.ops.NGinxRunOps import NGinxRunOps
-from nwm.ops.NGinxSetupOps import NGinxSetupOps
-from nwm.threads.NGinxRemoteThread import NGinxRemoteThread
 
 #___________________________________________________________________________________________________ NwmRecentWidget
 class NwmRecentWidget(PyGlassWidget):
@@ -35,95 +36,30 @@ class NwmRecentWidget(PyGlassWidget):
             AppConfigEnum.RECENT_PATHS, []
         )
 
+        topWidget, topLayout = self._createWidget(self, QtGui.QHBoxLayout, True)
+
+        label = QtGui.QLabel(topWidget)
+        label.setText(u'Select Recent Container Path:')
+        label.setStyleSheet("QLabel { color:#333; font-size:14px; }")
+        topLayout.addWidget(label)
+        topLayout.addStretch()
+
+        btn = PyGlassPushButton(
+            topWidget,
+            text='Cancel',
+            icon=IconSheetMap.CANCEL,
+            size=SizeEnum.SMALL,
+            colorScheme=ColorSchemes.COPPER
+        )
+        btn.setSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Preferred)
+        btn.clicked.connect(self._handleCancel)
+        topLayout.addWidget(btn)
+
         mainLayout.addStretch()
-
-#===================================================================================================
-#                                                                                   G E T / S E T
-
-#___________________________________________________________________________________________________ GS: rootPath
-    @property
-    def rootPath(self):
-        path = self._pathLineEdit.text()
-        if path:
-            return path
-        return NGinxSetupOps.getExePath()
-
-#===================================================================================================
-#                                                                                     P U B L I C
-
-#___________________________________________________________________________________________________ stopActiveServer
-    def stopActiveServer(self):
-        if not NGinxRunOps.isRunning() or self._serverThread is None:
-            return
-        NGinxRunOps.stopServer(self._serverThread.rootPath)
-
-#===================================================================================================
-#                                                                               P R O T E C T E D
-
-#___________________________________________________________________________________________________ _updateDisplay
-    def _updateDisplay(self):
-        self._startBtn.setEnabled(self._serverThread is None)
-        self._stopBtn.setEnabled(self._serverThread is not None)
-        self._reloadBtn.setEnabled(self._serverThread is not None)
-        self._pathLineEdit.setEnabled(self._serverThread is None)
 
 #===================================================================================================
 #                                                                                 H A N D L E R S
 
-#___________________________________________________________________________________________________ _handleStartServer
-    def _handleStartServer(self):
-        if not NGinxSetupOps.commandExists():
-            self.mainWindow.updateStatusBar(u'ERROR: No NGinx command available')
-            print 'NO COMMAND'
-            return
-
-        if NGinxRunOps.isRunning():
-            self.mainWindow.updateStatusBar(u'ERROR: An NGinx process is already active')
-            print 'ALREADY RUNNING'
-            return
-
-        thread = NGinxRemoteThread(self, rootPath=self.rootPath)
-        self._serverThread = thread
-        thread.start()
-        self.mainWindow.updateStatusBar(u'NGinx server now active')
-        self._updateDisplay()
-
-#___________________________________________________________________________________________________ _handleStopServer
-    def _handleStopServer(self):
-        if not self._serverThread:
-            self.mainWindow.updateStatusBar(u'ERROR: Server is not currently active')
-            print 'NO ACTIVE SERVER THREAD'
-            return
-
-        if not NGinxRunOps.isRunning():
-            self.mainWindow.updateStatusBar(u'ERROR: No active NGinx server found')
-            print 'NOT RUNNING'
-            return
-
-        NGinxRunOps.stopServer(self._serverThread.rootPath)
-        self._serverThread = None
-        self._updateDisplay()
-
-#___________________________________________________________________________________________________ _handleReloadServer
-    def _handleReloadServer(self):
-        if not NGinxRunOps.isRunning() or not self._serverThread:
-            return
-        NGinxRunOps.reloadServer(self._serverThread.rootPath)
-        self._updateDisplay()
-
-#___________________________________________________________________________________________________ _handleLocatePath
-    def _handleLocatePath(self):
-        self.refreshGui()
-        path = QtGui.QFileDialog.getExistingDirectory(
-            self,
-            caption=u'Specify Root NGinx Path',
-            dir=self.rootPath
-        )
-
-        if path:
-            self._pathLineEdit.setText(path)
-            self.mainWindow.appConfig.set(AppConfigEnum.ROOT_PATH, path)
-
-#___________________________________________________________________________________________________ _handleRecentLocations
-    def _handleRecentLocations(self):
-        pass
+#___________________________________________________________________________________________________ _handleCancel
+    def _handleCancel(self):
+        self.mainWindow.setActiveWidget('home')
