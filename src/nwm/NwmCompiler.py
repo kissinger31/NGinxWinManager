0bd3f5b588eb8a8911097540d93375313b599751
sernst <swernst@gmail.com>
2013-04-24 00:25:52 -0700

    Add NwmCompiler.py for executable compilation.

---
+++ src/nwm/NwmCompiler.py
@@ -0,0 +1,41 @@
+# NwmCompiler.py
+# (C)2013
+# Scott Ernst
+
+from pyglass.compile.PyGlassApplicationCompiler import PyGlassApplicationCompiler
+from pyglass.compile.SiteLibraryEnum import SiteLibraryEnum
+
+from nwm.NGinxWinManagerApplication import NGinxWinManagerApplication
+
+#___________________________________________________________________________________________________ NwmCompiler
+class NwmCompiler(PyGlassApplicationCompiler):
+    """A class for..."""
+
+#===================================================================================================
+#                                                                                   G E T / S E T
+
+#___________________________________________________________________________________________________ GS: siteLibraries
+    @property
+    def siteLibraries(self):
+        return [SiteLibraryEnum.PYSIDE]
+
+#___________________________________________________________________________________________________ GS: binPath
+    @property
+    def binPath(self):
+        return ['..', '..', 'bin']
+
+#___________________________________________________________________________________________________ GS: appFilename
+    @property
+    def appFilename(self):
+        return 'NGinxWinManager'
+
+#___________________________________________________________________________________________________ GS: applicationClass
+    @property
+    def applicationClass(self):
+        return NGinxWinManagerApplication
+
+####################################################################################################
+####################################################################################################
+
+if __name__ == '__main__':
+    NwmCompiler().run()
